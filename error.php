c1dac98277dcb74ef4affc916da181c4c4f94079
drimerdev <121459810+drimerdev@users.noreply.github.com>
2023-06-21 15:12:58 -0300

    Create error.php

---
+++ error.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Donation error</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        
+        .error-message {
+            color: red;
+            font-size: 20px;
+            margin-top: 50px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Ops!</h1>
+    <p class="error-message">Sorry, an error occurred while processing your donation. Please try again later.</p>
+</body>
+</html>
